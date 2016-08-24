1750e1ccfd72a9b73321070f96e2bdf989337424
PrachiGupta <prachi_gupta10@infosys.com>
2016-08-24 11:34:06 +0530

    First Commit ws3 11:33

---
+++ TestGitHelloW/src/com/eta/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package com.eta;
+
+public class TestHelloWorld {
+	
+	public static void greeting(){
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Greeting ...");
+		TestHelloWorld.greeting();
+	}
+
+}
